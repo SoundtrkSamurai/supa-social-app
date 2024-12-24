ef785db6b2a3fe4bc498076f00d2851058dad99c
SoundtrkSamurai <furtimat3@yahoo.com>
2024-12-23 20:04:13 -0800

    create welcome screen

---
+++ supa-social-app/helpers/common.ts
@@ -0,0 +1,7 @@
+import { Dimensions } from "react-native";
+
+const { width: deviceWidth, height: deviceHeight } = Dimensions.get('window');
+
+const hp = (percentage: number) => (percentage * deviceHeight) / 100;
+
+const wp = (percentage: number) => (percentage * deviceWidth) / 100;
