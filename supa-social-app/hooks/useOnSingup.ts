d529724097cc26d217062bbc43333c436b48c527
SoundtrkSamurai <furtimat3@yahoo.com>
2025-01-10 19:36:16 -0800

    auth: Wire up signup function

--- supa-social-app/hooks/useOnSingup.ts
+++ supa-social-app/hooks/useOnSingup.ts
@@ -1,16 +1,46 @@
-import { MutableRefObject, useCallback } from "react";
+import { supabase } from "@/lib/supabase";
+import { Dispatch, MutableRefObject, SetStateAction, useCallback } from "react";
 import { Alert } from "react-native";
 
-export function useOnSignUp({ emailRef, passwordRef, nameRef }: Params) {
-  return useCallback(() => {
-    if(!emailRef.current || !passwordRef.current || !nameRef.current) {
+export function useOnSignUp({
+  emailRef,
+  passwordRef,
+  nameRef,
+  setLoading,
+}: Params) {
+  return useCallback(async () => {
+    if (!emailRef.current || !passwordRef.current || !nameRef.current) {
       return Alert.alert("Login", "Please fill all the fields!");
     }
-  }, [emailRef, passwordRef, nameRef])
+
+    let name = nameRef.current.trim();
+    let email = emailRef.current.trim();
+    let password = passwordRef.current.trim();
+
+    setLoading(true);
+
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    
+    setLoading(false);
+
+    console.log('sessions: ',session);
+    console.log('error: ', error);
+
+    if (error) {
+      Alert.alert('Sign up error', error.message);
+    }
+  }, [emailRef, passwordRef, nameRef]);
 }
 
 type Params = Readonly<{
   emailRef: MutableRefObject<string>;
   passwordRef: MutableRefObject<string>;
   nameRef: MutableRefObject<string>;
-}>
+  setLoading: Dispatch<SetStateAction<boolean>>;
+}>;
